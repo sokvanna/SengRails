1bf93a8e5bc9d8c8a4d14c0cfe3171dc379c78ae
Sokvanna <sokvannas@gmail.com>
2014-01-29 21:59:48 -0500

    Auth

--- DragonStone/config/routes.rb
+++ DragonStone/config/routes.rb
@@ -1,6 +1,9 @@
 DragonStone::Application.routes.draw do
+devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' end
   get "pages/index"
   get "pages/about"
+  
+  root to: "home#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
